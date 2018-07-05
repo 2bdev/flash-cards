eb83a12335e2dd90bb05d4ec053e1107c1fb198d
Brian B <bbormandev@gmail.com>
2018-07-04 20:55:08 -0400

    Stubbed out ability to add collections, added more packages

--- src/components/CollectionList.js
+++ src/components/CollectionList.js
@@ -1,16 +1,52 @@
 import React, { Component } from 'react';
+import { Modal } from 'react-bootstrap';
+import update from 'immutability-helper';
 import CollectionListItem from './CollectionListItem';
+import CreateCollection from './CreateCollection';
+
+const collections = [
+  {
+    "title": "State Capitals",
+    "cardCount": 50
+  },
+  {
+    "title": "World Capitals",
+    "cardCount": 190
+  },
+  {
+    "title": "US Presidents",
+    "cardCount": 45
+  },
+];
 
 class CollectionList extends Component {
+  constructor(props) {
+		super(props);
+
+    this.state = {
+      collections: collections
+    };
+
+    this.onCreate = this.onCreate.bind(this);
+	}
+
+  onCreate(data) {
+    this.setState((prevState, props) => {
+      const newCollection = update(prevState.collections, {$push: [{title: data.newTitle, cardCount: 0}]});
+      return {
+        collections: newCollection
+      };
+    })
+  }
+
   render() {
+    const { collections } = this.state;
     return (
       <div className="collection-list">
-        <div className="create-collection">
-          <h3>Create a new collection</h3>
-        </div>
-				<CollectionListItem title="State Capitals" cardCount="50" />
-        <CollectionListItem title="World Capitals" cardCount="190" />
-        <CollectionListItem title="US Presidents" cardCount="45" />
+        <CreateCollection onCreate={this.onCreate} />
+        {collections.map(item => (
+            <CollectionListItem title={item.title} cardCount={item.cardCount} />
+        ))}
 			</div>
     );
   }
---
+++ src/components/CreateCollection.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import { Modal, Button, ControlLabel, FormControl } from 'react-bootstrap';
+
+class CreateCollection extends Component {
+  constructor(props) {
+		super(props);
+
+		this.state = {
+			show: false,
+      newTitle: '',
+		};
+
+		this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+    this.handleSaveClose = this.handleSaveClose.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleClose() {
+    this.setState({ show: false });
+  }
+
+  handleShow() {
+    this.setState({ show: true });
+  }
+
+  handleSaveClose() {
+    this.props.onCreate({
+      newTitle: this.state.newTitle
+    });
+    this.setState({
+      newTitle: ''
+    });
+    this.handleClose();
+  }
+
+  handleChange(e) {
+    this.setState({
+      newTitle: e.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div className="create-collection">
+        <h3 onClick={this.handleShow}>Create a new collection</h3>
+        <Modal show={this.state.show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Create Collection</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <ControlLabel>New Collection Title</ControlLabel>
+            <FormControl
+              type="text"
+              value={this.state.newTitle}
+              placeholder="New Collection"
+              onChange={this.handleChange}
+            />
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.handleClose}>Cancel</Button>
+            <Button onClick={this.handleSaveClose}>Create</Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default CreateCollection;
